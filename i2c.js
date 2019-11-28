e9b0f932e7f590b1ada335971bedb6b6daca4d3a
Sam Burkhard <samryanburk@gmail.com>
2019-11-28 05:48:23 +0000

    Added basic I2C code outside of module into js

--- i2c.js
+++ i2c.js
@@ -1,23 +1,69 @@
-var MPU6050 = require('i2c-mpu6050');
+//var MPU6050 = require('i2c-mpu6050');
 var i2c = require('i2c-bus');
+var async = require('async');
 
-const MPU_ADDR = 0x68;
-//const GYRO_X_REG = 0x43;
+var i2c1 = i2c.openSync(1);
 
-//const processData = rawData => {
-//	rawData = (rawData >>8) + ((rawData & 0xff) << 8);
-//	let value = (rawData & 0x0fff) / 16;
-//	return value;
-//};
+// Sensor https://www.invensense.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf
 
-//i2c.openPromisified(1).
-//then(i2c1 => i2c1.readWord(MPU_ADDR, GYRO_X_REG).
-//  then(rawData => console.log(processData(rawData))).
-//  then(_ => i2c1.close())
-//).catch(console.log);
+// Registers
+var PWR_MGMT_1 = 0x6b;
+var PWR_MGTM_2 = 0x6c;
+var ACL_XOUT = 0x3b;
+var ACL_YOUT = 0x3d;
+var ACL_ZOUT = 0x3f;
+var TMP_OUT = 0x41;
+var GYRO_XOUT = 0x43;
+var GYRO_YOUT = 0x45;
+var GYRO_ZOUT = 0x47;
 
-const i2c1 = i2c.openSync(1);
-const sensor = new MPU6050(i2c1, MPU_ADDR);
-const data = sensor.readSync();
-i2c1.closeSync();
+// Sensitivity
+var ACL_SENSITIVITY = 16384;
+var GYRO_SENSITIVITY = 131;
+
+// Address
+var address = 0x68;
+
+// Functions
+function scale(data, sensitivity) {
+  var scale = {};
+  for (var axis in data) {
+    scale[axis] = data[axis] / sensitivity;
+  }
+  return scale;
+};
+
+function read(register, done) {
+  var high = this.i2c1.readByteSync(this.address, register);
+  var low = this.i2c1.readByteSync(this.address, register + 1);
+  var data = (high << 8) + low;
+  if (data >- 0x8000) {
+    return -((65535 - data) + 1);
+  } else {
+    return data;
+  }
+};
+
+function readGyro() {
+    var json = {
+      x: this.read(GYRO_XOUT),
+      y: this.read(GYRO_YOUT),
+      z: this.read(GYRO_ZOUT)
+    };
+    json = scale(json, GYRO_SENSITIVITY);
+    return json;
+};
+
+function fetch() {
+  var gyro = this.readGyro();
+  return {
+    gyro: gyro
+  };
+};
+
+// Actual code that runs
+i2c1.writeByteSync(address, PWR_MGMT_1, 0);
+var data = this.fetch();
 console.log(data);
+console.log('Printed data');
+i2c1.closeSync();
