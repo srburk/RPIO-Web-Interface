6d694ff21635b1d79e509bb6d84572ab79adf228
Sam Burkhard <samryanburk@gmail.com>
2019-12-09 21:32:43 -0500

    Got Websocket broadcasting to multiple clients with basic html file

--- app.js
+++ app.js
@@ -3,23 +3,45 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var WebSocket = require('ws');
-
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 
 var app = express();
 
+// WebSocket Code =================================
+// import
+var WebSocket = require('ws');
+
 // initialize server instance
 const wss = new WebSocket.Server({ port: 8000 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+// on connection event, start this
+wss.on('connection', (ws) => {
+  // message handler
+  ws.on('message', (message) => {
     console.log('Recieved: %s', message);
   });
 
-  ws.send('Connected with WebSocket Server');
+  // status handlers
+  ws.send('Client connected');
+  console.log('Client connected');
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
 });
+
+// always send info regardless of client call requests
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
+
+// End WebSocket Code ==============================
+
+// Testing Client Code
+app.use('/client', express.static(path.join(__dirname, 'websocket-client.html')));
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
